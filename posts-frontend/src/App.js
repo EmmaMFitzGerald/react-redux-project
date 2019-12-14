63e8c6fb7cfbb0d40ea005ae6b6aa60eceb8add1
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-12-14 17:24:14 -0500

    added Users Component

--- posts-frontend/src/App.js
+++ posts-frontend/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {fetchUsers} from './actions/fetchUsers'
 import UsersContainer from './containers/UsersContainer'
 class App extends React.Component {
 
   render() {
     return(
       <div className="App">
-        App
         <UsersContainer/>
       </div>
     );
   }
 }
 
-export default connect()(App);
+export default App;
--- posts-frontend/src/containers/UsersContainer.js
+++ posts-frontend/src/containers/UsersContainer.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+import {fetchUsers} from '../actions/fetchUsers'
+import Users from '../components/Users'
+import UserInput from '../components/UserInput'
 
 class UsersContainer extends React.Component {
+
+    componentDidMount(){
+        this.props.fetchUsers()
+    }
 
     render(){
         return (
             <div>
-               UsersContainer 
+               <UserInput/>
+               <Users users={this.props.users}/>
             </div>
         )
     }
 }
 
-export default UsersContainer
+const mapStateToProps = state => {
+    return {
+        users: state.users
+    }
+}
+
+export default connect(mapStateToProps, {fetchUsers})(UsersContainer)
