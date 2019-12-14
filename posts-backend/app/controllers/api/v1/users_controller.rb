8b53e92066394a44b9ce9c1219f327a56f5faf10
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-12-14 13:15:25 -0500

    user controller added

--- posts-backend/app/controllers/api/v1/users_controller.rb
+++ posts-backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,33 @@
 class API::VI::UsersController < ApplicationController
+    
+    def index
+        @users = User.all 
+        render json :@users
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: :user
+        else
+            render json: {error: 'Error creating user'}
+        end 
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+    end
+
+    private
+    
+    def user_params 
+        params.require(:user).permit(:name)
+    end 
+
 end
