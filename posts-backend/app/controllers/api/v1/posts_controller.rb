e91ed050c5d1a00de0a1c6d553fc4d529bd9e7d1
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-12-14 13:49:09 -0500

    posts controller set up

--- posts-backend/app/controllers/api/v1/posts_controller.rb
+++ posts-backend/app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,32 @@
 class API::VI::PostsController < ApplicationController
+
+    before_action :set_user
+
+    def index
+       @posts = @user.posts
+       render json: @posts
+    end
+
+    def show
+        @post = Post.find(param[:id])
+        render json: @post
+    end
+
+    def create
+        @post = @user.posts.new(post_params)
+        if @post.save
+            render json :@post
+        else 
+            render json: {error: 'Error saving post'}
+        end
+    end 
+
+    def set_user
+        @user = User.find(params[:user_id])
+    end 
+    
+    def post_params 
+        params.require(:post).permit(:title, :category, :content)
+    end 
+
 end
